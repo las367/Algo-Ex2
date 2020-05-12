0ac4f0c5b05131204eb274996557ff63acce3f88
Louis <louisandrew3@gmail.com>
2020-05-12 16:15:21 +0200

    implements bubble to be more efficient

--- src/main/java/Algo/Ex2/sort/BubbleSort.java
+++ src/main/java/Algo/Ex2/sort/BubbleSort.java
@@ -9,14 +9,19 @@ public class BubbleSort<T> extends Swapper<T> implements Sortable<T> {
     public void sort(Listable<T> list, Comparator<T> comparator) {
         
         //moves from left to right
-        for ( int i = 1; i < list.size(); i++ ) {
+        boolean swapped = true;
+        for ( int i = 1; swapped && i < list.size(); i++ ) {
 
+            //swapped => we don't have to go through the array when the array is already sorted.
+            //swap flag => on best case => time complexity = O(n)
+            swapped = false;
             //moves from left to right, everytime element e is bigger than e + 1 => swap the element
             for ( int j = 0; j < list.size() - i; j++ ) {
                 
                 if ( comparator.compare(list.get(j), list.get(j+1)) > 0 ) {
                     swap( list, j, j+1 );
                     //at the end of every step => the largest element would be at the far right
+                    swapped = true;
                 }
             }
         }
